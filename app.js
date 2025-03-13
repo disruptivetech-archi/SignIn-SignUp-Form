dbff371ca63dcdd5ff9da436d3ddfb0538481a6d
Disruptive Technologies <disruptivetechnologies@architecht.com>
2025-03-13 10:45:18 +0300

    Feat: Forget Password Frontend Addition

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+const forgot_password_link = document.querySelector("#forgot-password-link");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener('click', () =>{
@@ -8,4 +9,9 @@ sign_up_btn.addEventListener('click', () =>{
 
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
+});
+
+forgot_password_link.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.add("forgot-password-mode");
 });
